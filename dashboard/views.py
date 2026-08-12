19d290bb33f01dadae74e8aa012cac901c89f0eb
Doménica Bolaños <dbolanos@espol.edu.ec>
2026-08-12 20:41:05 +0000

    cambios

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-def index(request):
-    data = {
-        "title": "Landing Page' Dashboard",
-    }
 
-    return render(request, "dashboard/index.html", data)
+def index(request):
+    return render(request, "dashboard/index.html")
